dfe25447a2f76b340367be8ed09a56e13cc7a7ab
Ethan Cheam <ethan.cheam123@gmail.com>
2025-04-12 13:07:56 +0100

    Implemented price filter

--- src/pages/CookiePage.tsx
+++ src/pages/CookiePage.tsx
@@ -13,7 +13,7 @@ function CookiePage() {
     const maxPrice = 100
     const minPrice = 0
     const handleSliderUpdate = (event: Event, newPriceRange: (number | string)[]) => {
-        console.log(newPriceRange)
+        console.log('npr' + newPriceRange)
         let min = priceRange[0]
         let max = priceRange[1]
         if (newPriceRange[0] !== '' && newPriceRange[1] !==  ''){
@@ -23,18 +23,8 @@ function CookiePage() {
         }
         setDisplayRange([min, max])
     }
-    const handleMinChange = (event: Event) =>{
-        // Need to explicitly convert to number
-        // setPriceRange(event.target.value === null ? [textFieldRange[0], textFieldRange[1]] : [Math.max(0, Number(event.target.value)), priceRange[1]])
-        // Empty textfield in number mode is '' not null
-        console.log(event.target.value)
-        setDisplayRange(event.target.value === '' ? ['', displayRange[1]] : [Number(event.target.value), displayRange[1]])
-        // Below would interpret '' as 0 preventing backspace
-        // setTextFieldRange([Number(event.target.value), textFieldRange[1]])
-    }
-    const handleOnBlue = () =>{
-        console.log("BLurre")
-    }
+
+
 
     // create a slider
     // have value be changes
@@ -44,34 +34,24 @@ function CookiePage() {
         <>
             <Header/>
             <div>CookiePage</div>
-            <Box sx={{display:"flex", width: "100%", minHeight: "800px", padding: "10px", background: "beige"}}>
-                <Box sx={{width: "200px"}}>
+            <Box sx={{display:"flex", width: "100%", minHeight: "800px", padding: "20px", background: "beige", }}>
+                <Box sx={{width: "220px", padding:"5px"}}>
                     <Typography variant="body1" fontSize={24}>Filter by</Typography>
                     <hr></hr>
-                    <Grid2 container spacing={2} sx={{alignItems: 'center'}}>
-                        <Grid2>
-                            <Input 
-                            value={priceRange[0]}
-                            // defaultValue={''}
-                            size="small"
-                            onChange={handleMinChange}
-                            // onSubmit only works as part of forms
-                            // onSubmit={handleMinChange}
-                            // Text field supports backspacing the entire number while Input with type="number" always starts with
-                            // value={priceRange[0] === 0 ? '' : priceRange[0]}
-                            // defaultValue={minPrice} cannot bave this and value at the same time. controlled vs uncontrolled.
-                            type="number"
-                            />
-                            <TextField type="number" value={displayRange[0]} onChange={(e:Event) => {setDisplayRange([e.target.value, displayRange[1]])}} onBlur={()=>handleSliderUpdate(null, displayRange)}/>
-                            
-                        </Grid2>
-                    </Grid2>
-
-                    <Slider
+                    <Typography variant="body1" fontSize={16}>Price</Typography>
+                    <Box sx={{display:"flex", justifyContent:"center"}}>
+                        <Slider
                         getAriaLabel={() => 'Select maximum and minimum price'}
                         value={priceRange}
                         onChange={handleSliderUpdate}
-                    />
+                        sx={{width:"180px"}}
+                        disableSwap
+                        />
+                    </Box>
+                    <Box sx={{display:"flex", wrap:"nowrap", marginTop:"15px", justifyContent:"space-between"}}>
+                            <TextField label="Min" variant="standard" type="number" value={displayRange[0]} onChange={(e:Event) => {setDisplayRange([e.target.value, displayRange[0]])}} onBlur={()=>handleSliderUpdate(null, displayRange)} sx={{width:"50px"}}/>
+                            <TextField label="Max" variant="standard" type="number" value={displayRange[1]} onChange={(e:Event) => {setDisplayRange([displayRange[0], e.target.value])}} onBlur={()=>handleSliderUpdate(null, displayRange)} sx={{width:"50px"}}/>
+                    </Box>
                 </Box>
             </Box>
         </>
